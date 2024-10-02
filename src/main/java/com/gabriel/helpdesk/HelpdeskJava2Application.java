3381687efcb9d8898b02bd4a76a1246487797c4a
gabrielprogramadorweb <gabriel.programador.ads@gmail.com>
2024-10-02 15:13:59 -0300

    primeiro

---
+++ src/main/java/com/gabriel/helpdesk/HelpdeskJava2Application.java
@@ -0,0 +1,13 @@
+package com.gabriel.helpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpdeskJava2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpdeskJava2Application.class, args);
+	}
+
+}
