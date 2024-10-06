d6e1359d7aa4cc05aa5be8159f770266fd36e062
gabrielprogramadorweb <gabriel.programador.ads@gmail.com>
2024-10-06 16:13:05 -0300

    [main] - Implementando findAll

--- src/main/java/com/gabriel/helpdesk/resources/TecnicoResource.java
+++ src/main/java/com/gabriel/helpdesk/resources/TecnicoResource.java
@@ -1,5 +1,8 @@
 package com.gabriel.helpdesk.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,21 +11,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gabriel.helpdesk.domain.Tecnico;
+import com.gabriel.helpdesk.domain.dtos.TecnicoDTO;
 import com.gabriel.helpdesk.services.TecnicoService;
 
 @RestController
 @RequestMapping(value = "/tecnicos")
 public class TecnicoResource {
 		
-	// localhost:8081/tecnicos
-	
 	@Autowired
 	private TecnicoService service;
 	
+	// Endpoint para buscar Técnico por ID
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Tecnico> findById(@PathVariable Integer id) {
+	public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id) {
 		Tecnico obj = service.findById(id);
-		return ResponseEntity.ok().body(obj);
-		
+		return ResponseEntity.ok().body(new TecnicoDTO(obj));
+	}
+	
+	// Endpoint para buscar todos os Técnicos
+	@GetMapping
+	public ResponseEntity<List<TecnicoDTO>> findAll() {
+		List<Tecnico> list = service.findAll();
+		List<TecnicoDTO> listDTO = list.stream().map(obj -> new TecnicoDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 }
